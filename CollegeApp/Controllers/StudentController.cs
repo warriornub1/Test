1088769f05ef447d21817164bda7902fed558145
warriornub <cmpqwdoq@gmail.com>
2024-12-04 23:15:24 +0800

    first commit

---
+++ CollegeApp/Controllers/StudentController.cs
@@ -0,0 +1,63 @@
+﻿using CollegeApp.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace CollegeApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        [Route("All", Name = "GetAllStudents")]
+        public ActionResult<IEnumerable<Student>> GetStudentName()
+        {
+            return Ok(CollegeRepository.Students);
+        }
+
+        [HttpGet]
+        [Route("{id:int}", Name = "GetStudentsById")]
+        public ActionResult<Student> GetStudentById(int id)
+        {
+            if (id <= 0)
+                return BadRequest();
+
+            var student = CollegeRepository.Students.Where(x => x.Id == id).FirstOrDefault();
+            if (student == null)
+                return NotFound($"The student with id {id} not found.");
+
+            return Ok(student);
+        }
+
+
+        [HttpGet]
+        [Route("{Name:alpha}", Name = "GetStudentByName")]
+        public ActionResult<Student> GetStudentByName(string Name)
+        {
+            if(string.IsNullOrEmpty(Name))
+                return BadRequest();
+
+            var student = CollegeRepository.Students.Where(x => x.StudentName == Name).FirstOrDefault();
+            
+            if(student == null)
+                return NotFound($"The student with name {Name} not found.");
+
+            return Ok(student) ;
+        }
+
+
+        [HttpDelete("{id:min(1):max(100)}", Name = "DeleteStudentById")]
+        public ActionResult DeleteStudent(int id)
+        {
+            var student = CollegeRepository.Students.Where(x => x.Id == id).FirstOrDefault();
+
+            if (student == null)
+                return NotFound($"The student with id {id} not found.");
+
+
+            CollegeRepository.Students.Remove(student);
+
+            return Ok(student);
+        }
+    }
+}
---
+++ CollegeApp/Model/CollegeRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace CollegeApp.Model
+{
+    public static class CollegeRepository
+    {
+        public static List<Student> Students { get; set; } = new List<Student>()
+        {
+            new Student
+            {
+                Id = 1,
+                StudentName = "Student 1",
+                Email = "Studentemail@gmail.com",
+                Address = "Hyd, India"
+            },
+            new Student
+            {
+                Id = 2,
+                StudentName = "Student 2",
+                Email = "Student2email@gmail.com",
+                Address = "Hyd, India"
+            }
+        };
+    }
+}
